d2e290046c8bd26df0d1e330aea83a5b45fb27c5
jhj <quantumleapj@gmail.com>
2022-02-03 17:49:27 +0900

    22.02.03 수업끝

--- practice2/config/settings.py
+++ practice2/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t9s+p=!v+s$qmv29r_8i5eo4guml!60!04+8-e@o+i&#a8@1g!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','192.168.35.251']
 
 
 # Application definition
